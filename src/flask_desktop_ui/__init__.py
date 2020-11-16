39db82527ec3bc3bd23a14d294ffaf90f16b9831
Javier Moragon <jamofer@gmail.com>
2020-11-16 06:36:19 +0100

    Allocate random port for flask backend

--- src/flask_desktop_ui/__init__.py
+++ src/flask_desktop_ui/__init__.py
@@ -1,18 +1,29 @@
+import socket
 from threading import Thread
 
 from flask_desktop_ui import chromium_browser_wrapper
 
 LOCAL_HOST = '127.0.0.1'
-FLASK_PORT = 62191
+RANDOM_PORT = 0
 
 
 class FlaskDesktopUI(object):
     def __init__(self, app):
-        self.flask_job = Thread(target=app.run, args=(LOCAL_HOST, FLASK_PORT))
+        self.port = _get_random_port()
+        self.flask_job = Thread(target=app.run, args=(LOCAL_HOST, self.port))
         self.flask_job.daemon = True
 
     def run(self):
         self.flask_job.start()
         chromium_browser_wrapper.initialize()
-        chromium_browser_wrapper.open_url('http://{ip}:{port}'.format(ip=LOCAL_HOST, port=FLASK_PORT))
+        chromium_browser_wrapper.open_url('http://{ip}:{port}'.format(ip=LOCAL_HOST, port=self.port))
         chromium_browser_wrapper.message_loop()
+
+
+def _get_random_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((LOCAL_HOST, RANDOM_PORT))
+    port = sock.getsockname()[1]
+    sock.close()
+
+    return port
--- test/test_flask_desktop_ui.py
+++ test/test_flask_desktop_ui.py
@@ -1,3 +1,4 @@
+import socket
 import unittest
 from time import sleep
 
@@ -12,21 +13,27 @@ class TestFlaskDesktopUI(unittest.TestCase):
         self.initialize = patch('flask_desktop_ui.chromium_browser_wrapper.initialize').start()
         self.open_url = patch('flask_desktop_ui.chromium_browser_wrapper.open_url').start()
         self.message_loop = patch('flask_desktop_ui.chromium_browser_wrapper.message_loop').start()
+        self.socket = patch('socket.socket').start()
 
     def tearDown(self):
         patch.stopall()
 
     def test_it_runs_flask_desktop_ui(self):
+        sock = MagicMock()
+        sock.getsockname.return_value = ('127.0.0.1', 10000)
+        self.socket.return_value = sock
         app = MagicMock()
         desktop_app = flask_desktop_ui.FlaskDesktopUI(app)
 
         desktop_app.run()
 
         wait_until_called(app.run)
-        app.run.assert_called_once_with(flask_desktop_ui.LOCAL_HOST, flask_desktop_ui.FLASK_PORT)
+        app.run.assert_called_once_with(flask_desktop_ui.LOCAL_HOST, 10000)
         self.initialize.assert_called_once()
+        self.socket.assert_called_once_with(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind.assert_called_once_with((flask_desktop_ui.LOCAL_HOST, flask_desktop_ui.RANDOM_PORT))
         self.open_url.assert_called_once_with(
-            'http://{ip}:{port}'.format(ip=flask_desktop_ui.LOCAL_HOST, port=flask_desktop_ui.FLASK_PORT)
+            'http://{ip}:{port}'.format(ip=flask_desktop_ui.LOCAL_HOST, port=10000)
         )
         self.message_loop.assert_called_once()
 
